19c6b66e7ebf8b198288ab941b696513517d8a1b
Nikolaus Walton <nikawalton2@gmail.com>
2023-05-25 18:44:45 -0700

    finish writing tests for start of tuple class

--- tests/main_tests.cpp
+++ tests/main_tests.cpp
@@ -9,10 +9,9 @@ TEST (TuplePointVectorTests, TupleCreationTest_IsVector) {
     EXPECT_EQ(tuple.GetX(), 4.3);
     EXPECT_EQ(tuple.GetY(), -4.2);
     EXPECT_EQ(tuple.GetZ(), 3.1);
-    EXPECT_EQ(tuple.IsVector(), 0.0);
+    EXPECT_EQ(tuple.IsPoint(), 0.0);
 
 }
-
 
 TEST (TuplePointVectorTests, TupleCreationTest_IsPoint) {
     Tuple tuple(4.3, -4.2, 3.1, 1.0);
@@ -20,6 +19,24 @@ TEST (TuplePointVectorTests, TupleCreationTest_IsPoint) {
     EXPECT_EQ(tuple.GetX(), 4.3);
     EXPECT_EQ(tuple.GetY(), -4.2);
     EXPECT_EQ(tuple.GetZ(), 3.1);
-    EXPECT_EQ(tuple.IsVector(), 1.0);
+    EXPECT_EQ(tuple.IsPoint(), 1.0);
 
+}
+
+TEST (TuplePointVectorTests, PointFactoryTest) {
+    Tuple factory;
+    Tuple point = factory.Point(4, -4, 3);
+
+    EXPECT_EQ(point.GetX(), 4);
+    EXPECT_EQ(point.GetY(), -4);
+    EXPECT_EQ(point.GetZ(), 3);
+    EXPECT_EQ(point.IsPoint(), 1);
+}
+
+TEST (TuplePointVectorTests, VectorFactoryTest) {
+
+    EXPECT_EQ(point.GetX(), 4);
+    EXPECT_EQ(point.GetY(), -4);
+    EXPECT_EQ(point.GetZ(), 3);
+    EXPECT_EQ(point.IsPoint(), 0);
 }
